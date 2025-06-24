4e6bba920fb026b0c073262ef149022e50af7a9e
crossbat <xowls1999@gmail.com>
2025-06-24 22:27:52 +0900

    feat: header, footer 공용 컴포넌트 제작 fix: packages/ui에 대한 tailwind적용 안되는 문제 해결

---
+++ apps/main/src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@repo/ui/Header"
+import { Footer } from "@repo/ui/Footer"
+
+export const Main = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
