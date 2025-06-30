a43289c933d342a405aba11a601d41131d784676
nambin <mkkim044@naver.com>
2025-06-30 14:53:02 +0900

    chore : 병합 전 커밋

--- apps/payment/src/pages/PaymentPage.tsx
+++ apps/payment/src/pages/PaymentPage.tsx
@@ -6,6 +6,8 @@ import PaySelect from '../components/paymentPage/PaySelect';
 import PaymentFinal from '../components/paymentPage/PaymentFinal';
 
 const PaymentPage = () => {
+  // TODO : 기능 구현해야함 카드나 계좌이체 모달 선택 후 지도 입력이 뜨도록
+
   const subText = ['선물 정보', '추가 후원금', '후원자 정보', '결제 수단'];
 
   return (
