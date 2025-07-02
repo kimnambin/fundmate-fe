6c5c5154305bff273e76211dfc5b401e12d5c737
nambin <mkkim044@naver.com>
2025-07-02 12:23:50 +0900

    merge : develop 병합 전 임시 커밋

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,4 +1,43 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import './index.css';
+// import { Main } from './pages/Main';
+// import { SearchPage } from './pages/SearchDivider';
+// import { Header } from '@repo/ui/components';
+// import { LogIn } from './pages/LogIn';
+// import { SignUp } from './pages/SignUp';
+// import { PasswordReset } from './pages/PasswordReset';
+// import { lazy } from 'react';
+
+// const CreateFundingPage = lazy(() => import('funding/CreateFundingPage'));
+// const ProductPage = lazy(() => import('payment/ProductPage'));
+// const PaymentPage = lazy(() => import('payment/PaymentPage'));
+// const PaymentCompleted = lazy(() => import('payment/PaymentCompleted'));
+// const PaymentDetail = lazy(() => import('payment/PaymentDetail'));
+// const PaymentListPage = lazy(() => import('payment/PaymentListPage'));
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Main />} />
+//         <Route path="/search" element={<SearchPage />} />
+//         <Route path="/login" element={<LogIn />} />
+//         <Route path="/signup" element={<SignUp />} />
+//         <Route path="/reset" element={<PasswordReset />} />
+//         <Route path="/funding/create" element={<CreateFundingPage />} />
+//         <Route path="/product" element={<ProductPage />} />
+//         <Route path="/payment" element={<PaymentPage />} />
+//         <Route path="/payment-completed" element={<PaymentCompleted />} />
+//         <Route path="/payment-detail" element={<PaymentDetail />} />
+//         <Route path="/payment-list" element={<PaymentListPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import './index.css';
 import { Main } from './pages/Main';
 import { SearchPage } from './pages/SearchDivider';
@@ -6,7 +45,8 @@ import { Header } from '@repo/ui/components';
 import { LogIn } from './pages/LogIn';
 import { SignUp } from './pages/SignUp';
 import { PasswordReset } from './pages/PasswordReset';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 const CreateFundingPage = lazy(() => import('funding/CreateFundingPage'));
 const ProductPage = lazy(() => import('payment/ProductPage'));
@@ -17,22 +57,24 @@ const PaymentListPage = lazy(() => import('payment/PaymentListPage'));
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset" element={<PasswordReset />} />
-        <Route path="/funding/create" element={<CreateFundingPage />} />
-        <Route path="/product" element={<ProductPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/payment-completed" element={<PaymentCompleted />} />
-        <Route path="/payment-detail" element={<PaymentDetail />} />
-        <Route path="/payment-list" element={<PaymentListPage />} />
-      </Routes>
-    </BrowserRouter>
+      <Suspense fallback={<>로딩 중...</>}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/reset" element={<PasswordReset />} />
+          <Route path="/funding/create" element={<CreateFundingPage />} />
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/payment-completed" element={<PaymentCompleted />} />
+          <Route path="/payment-detail" element={<PaymentDetail />} />
+          <Route path="/payment-list" element={<PaymentListPage />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
