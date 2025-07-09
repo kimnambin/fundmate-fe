86400553277e555f93cd4b34deebcda5f0673fb5
crossbat <xowls1999@gmail.com>
2025-07-09 10:40:48 +0900

    isLoading 레이아웃 버그 해결

--- apps/statistics/src/pages/Statistics.tsx
+++ apps/statistics/src/pages/Statistics.tsx
@@ -1,27 +1,24 @@
 import { StatisticsBody } from '../components/StatisticsBody';
 import { StatisticsHeader } from '../components/StatisticsHeader';
 import { Layout } from '@repo/ui/styles'
-import { Loading, Modal } from '@repo/ui/components';
+import { Loading } from '@repo/ui/components';
 import { statisticsStore } from '../stores/StatisticsStore';
 
 const StatisticsPage = () => {
   const isLoading = statisticsStore((state) => state.isLoading);
   const isSubmit = statisticsStore((state) => state.isSubmit);
 
-  if (isLoading) {
-    return (
-      <Layout>
-        <Modal isOpen={isLoading}>
-          <Loading />
-        </Modal>
-      </Layout>
-    )
-  }
-
   return (
     <Layout>
       <div className='flex flex-col gap-10'>
         <StatisticsHeader />
+        {
+          isLoading && (
+            <Layout>
+              <Loading />
+            </Layout>
+          )
+        }
         {
           isSubmit && <StatisticsBody />
         }
