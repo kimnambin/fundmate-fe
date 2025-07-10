90e147174a2981aea887ef82ff7da7ee3146c1ca
mingyeong0210 <mica7150@naver.com>
2025-07-10 15:31:06 +0900

    style: main에 md 에디터를 위한 스타일 추가

--- apps/main/src/styles/global.ts
+++ apps/main/src/styles/global.ts
@@ -10,6 +10,17 @@ const GlobalStyle = createGlobalStyle`
         font-family: 'Pretendard';
         font-size: 14px;
     }
+
+    .wmde-markdown ul,
+    .wmde-markdown ol {
+    margin-left: 1.25rem;
+    padding-left: 1rem;
+    list-style-type: disc;
+    }
+
+    .wmde-markdown ol {
+    list-style-type: decimal;
+}
 `;
 
 export default GlobalStyle;
