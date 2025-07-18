e5f8fce1e1e79ee25573dc8f7674e9ee46b7ba5c
crossbat <xowls1999@gmail.com>
2025-07-18 19:34:19 +0900

    데이터 가공 중

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
-import dotenv from 'dotenv'
-import { resolve } from 'path'
+import dotenv from 'dotenv';
+import { resolve } from 'path';
 
-dotenv.config({ path: resolve(__dirname, '../../.env') })
+dotenv.config({ path: resolve(__dirname, '../../.env') });
+
+const ADMIN = 'https://fundmate-fe-admin.vercel.app';
+const FUNDING = 'https://fundmate-fe-funding.vercel.app';
+const MYPAGE = 'https://fundmate-fe-mypage.vercel.app';
+const PAYMENT = 'https://fundmate-fe-payment.vercel.app';
+const STATISTICS = 'https://fundmate-fe-statistics.vercel.app';
+
+const isDeployed = import.meta.env.MODE === 'production';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,11 +21,21 @@ export default defineConfig({
     federation({
       name: 'main',
       remotes: {
-        admin: 'http://localhost:5001/assets/remoteEntry.js',
-        funding: 'http://localhost:5002/assets/remoteEntry.js',
-        mypage: 'http://localhost:5003/assets/remoteEntry.js',
-        payment: 'http://localhost:5004/assets/remoteEntry.js',
-        statistics: 'http://localhost:5005/assets/remoteEntry.js',
+        admin: isDeployed
+          ? ADMIN
+          : 'http://localhost:5001/assets/remoteEntry.js',
+        funding: isDeployed
+          ? FUNDING
+          : 'http://localhost:5002/assets/remoteEntry.js',
+        mypage: isDeployed
+          ? MYPAGE
+          : 'http://localhost:5003/assets/remoteEntry.js',
+        payment: isDeployed
+          ? PAYMENT
+          : 'http://localhost:5004/assets/remoteEntry.js',
+        statistics: isDeployed
+          ? STATISTICS
+          : 'http://localhost:5005/assets/remoteEntry.js',
       },
       shared: [
         'react',
@@ -44,12 +62,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.BACKEND_ADDRESS,
+        target: process.env.VITE_BACKEND_ADDRESS,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
-        cookieDomainRewrite: "",
+        cookieDomainRewrite: '',
       },
-    }
-  }
+    },
+  },
 });
