8c92982433ce3d6cb3d686a7074867a322dfaa21
kim song yun <rlatjddbs531@naver.com>
2025-07-19 10:51:57 +0900

    작업 중인 App.tsx 변경사항 커밋

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header, UserPageLayout } from '@repo/ui/components';
 import FundingHistory from './pages/fundingHistory/fundingHistory';
 import PaymentManagement from './pages/paymentManagement/paymentManagement';
-import MakerProfile from './pages/makerProfile/makerProfile';
 import StatsPage from './pages/stats/statsPage';
 import { Layout } from '@repo/ui/styles';
 
@@ -19,7 +18,6 @@ const App = () => (
       <Route element={<Layout />}>
         <Route path="/userprofile-settings" element={<div>유저 프로필 설정 페이지</div>} />
         <Route path="/withdrawal" element={<div>회원 탈퇴 페이지</div>} />
-        <Route path="/makerprofile" element={<MakerProfile nickname='tempData' />} />
       </Route>
     </Routes>
   </BrowserRouter>
