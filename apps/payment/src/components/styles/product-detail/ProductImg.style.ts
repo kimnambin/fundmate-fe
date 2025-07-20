c423b4190672ed561f6d912caf63604274a51bf2
mingyeong0210 <mica7150@naver.com>
2025-07-20 16:03:56 +0900

    chore: 불필요 주석 제거

--- apps/payment/src/components/styles/product-detail/ProductImg.style.ts
+++ apps/payment/src/components/styles/product-detail/ProductImg.style.ts
@@ -1,22 +1,5 @@
 import tw from 'tailwind-styled-components';
 
-// export const Wrapper = tw.div`
-//   w-[90%]
-//   h-[80%]
-//   flex
-//   flex-col
-//   items-start
-//   gap-4
-//   mt-8
-// `;
-
-// export const MainImg = tw.img`
-//     w-full
-//     h-[85%]
-//     max-h-[600px]
-//     object-cover
-// `;
-
 export const Wrapper = tw.div`
   w-[90%]
   h-[80%]
@@ -25,8 +8,8 @@ export const Wrapper = tw.div`
   items-start
   gap-4
   mt-8
-  max-h-[600px]       /* 최대 높이 제한 */
-  min-h-[200px]       /* 최소 높이 지정해서 너무 작아지는 것도 방지 */
+  max-h-[600px]      
+  min-h-[200px]       
 `;
 
 export const MainImg = tw.img`
@@ -34,6 +17,6 @@ export const MainImg = tw.img`
   h-[85%]
   max-h-[600px]
   object-cover
-  rounded-md          /* 혹시 모서리 둥글게 */
-  transition-all      /* 부드러운 크기 변화 */
+  rounded-md          
+  transition-all    
 `;
